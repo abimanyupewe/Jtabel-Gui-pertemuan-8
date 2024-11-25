1decfb6005f6a2c9fb828c4c51877ce3b056e2fa
abimanyupewe <abimanyupw369@gmail.com>
2024-11-25 19:46:40 +0700

    pertemuan 8 jTable

---
+++ pertemuan8/src/main/java/com/mycompany/pertemuan8/Pertemuan8.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan8;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Pertemuan8 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
